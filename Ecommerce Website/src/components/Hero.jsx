486946d2a2725edf28d9e7c9a19debd907299670
rehanmirza76 <abdulrehan0317@gmail.com>
2024-06-05 17:09:52 +0500

    commit

--- Ecommerce Website/src/components/Hero.jsx
+++ Ecommerce Website/src/components/Hero.jsx
@@ -12,14 +12,14 @@ const Hero = () => {
           alt=""
         />
       </div>
-      <div className="cont  flex   justify-around  text-left absolute md:top-[5%] top-[10%] items-center p-[5%] md:justify-between h-auto w-[90vw]  z-30">
+      <div className="cont  flex   justify-around  text-left absolute md:top-[1%] top-[10%] items-center p-[5%] md:justify-between h-auto w-[90vw]  z-30">
         <div className="first text-[7px]  md:text-xl w-[50vw] flex flex-col items-start">
-          <h1 className="text-base md:text-5xl font-bold ">
-            WELCOME TO THE STORE
+          <h1 className="text-base md:text-7xl font-bold w-[70%] my-2 ">
+            70% OFF on all products Sale
           </h1>
           Lorem ipsum dolor, sit amet consectetur Lorem ipsum dolor sit amet
           consectetur adipisicing elit. Blanditiis, iste?
-          <button className="bg-green-500 py-[2px] px-[3px] md:px-5 md:py-2 md:mt-2 mt-[2px] rounded-sm">
+          <button className="bg-green-500 py-[3px] px-[7px] md:px-5 my-5 md:py-2 md:mt-2 mt-[2px] rounded-md">
             Buy Now
           </button>
         </div>
