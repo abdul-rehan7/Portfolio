a1a6ec3f7501dba08a8718262a6aca130fd9f46d
rehanmirza76 <abdulrehan0317@gmail.com>
2024-07-03 21:53:13 +0500

    Update

--- shopping-website/src/app/components/NavBar.tsx
+++ shopping-website/src/app/components/NavBar.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
 import { BsSearch } from "react-icons/bs";
+import { BiUser } from "react-icons/bi";
+import { FiHeart } from "react-icons/fi";
+import { HiOutlineShoppingBag } from "react-icons/hi";
+import Link from "next/link";
 
 const NavBar = () => {
   return (
@@ -47,33 +51,64 @@ const NavBar = () => {
                 <option value="French">French</option>
                 <option value="German">German</option>
               </select>
+            <div className="theme-btn">
+              <div className="cir"></div>
+            </div>
             </div>
           </div>
         </div>
       </div>
       {/* MIDDLE NAVBAR */}
-      <div className="border-b border-gray-300 py-6">
+      <div className="border-b border-gray-300 py-6 px-4">
         <div className="container sm:flex justify-between items-center">
-          <div className="text-3xl font-bold pb-4 sm:pb-0 text-[var(--third)]">
+          <div className="text-3xl text-center font-bold pb-4 sm:pb-0 text-[var(--third)]">
             ShopInn
           </div>
-          <div className="w-full sm:w-[300px] md:w-[70%]  relative pb-4 sm:pb-0 text-[var(--third)]">
-            <input className="p-2 px-4 border rounded-lg w-full border-gray-300" type="search" placeholder="Search..." />
-            <BsSearch className="absolute right-0 top-0 mr-3 mt-3  text-gray-400"/>
+          <div className="w-full sm:w-[300px] md:w-[70%] relative pb-4 sm:pb-0 text-[var(--third)]">
+            <input
+              className="p-2 px-4 border rounded-lg w-full border-gray-300"
+              type="search"
+              placeholder="Search..."
+            />
+            <BsSearch className="hidden right-0 top-0 mr-3 mt-3  text-gray-400" />
+          </div>
+
+          <div className="hidden lg:flex gap-4 test-gray-500 text-[1.5rem]">
+            <BiUser />
+            <FiHeart />
+            <HiOutlineShoppingBag />
           </div>
         </div>
       </div>
-      {/* BOTTOM NAVBAR */}
-      <div className="border-b font-medium text-gray-400 border-gray-300 py-4 px-[5rem]">
-        <div className="container  sm:flex justify-center gap-7 items-center">
-          <a href="">HOME</a>
-          <a href="">CATEGORIES</a>
-          <a href="">MEN'S</a>
-          <a href="">WOMEN'S</a>
-          <a href="">JEWELRY</a>
-          <a href="">PERFUME</a>
-          <a href="">BLOG</a>
-          <a href="">HOT OFFERS</a>
+      {/* LOWER NAVBAR  */}
+      <div className="hidden lg:block ">
+        <div className="container">
+          <div className="flex w-fit mx-auto gap-10 py-4">
+            <Link href="#" className="Nav_Link relative">
+              HOME
+            </Link>
+            <Link href="#" className="Nav_Link relative">
+              CATOGORIES
+            </Link>
+            <Link href="#" className="Nav_Link relative">
+              MEN'S
+            </Link>
+            <Link href="#" className="Nav_Link relative">
+              WOMEN'S
+            </Link>
+            <Link href="#" className="Nav_Link relative">
+              JEWELRY
+            </Link>
+            <Link href="#" className="Nav_Link relative">
+              PERFUME
+            </Link>
+            <Link href="#" className="Nav_Link relative">
+              BLOG
+            </Link>
+            <Link href="#" className="Nav_Link relative">
+              HOT OFFERS
+            </Link>
+          </div>
         </div>
       </div>
     </>
