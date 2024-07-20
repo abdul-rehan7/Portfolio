db381bde99a8e6a5012cc22c19a3e555bcefe0eb
rehanmirza76 <abdulrehan0317@gmail.com>
2024-07-21 00:04:30 +0500

    update

--- portfolio-web/components/ui/BentoGrid.tsx
+++ portfolio-web/components/ui/BentoGrid.tsx
@@ -25,11 +25,11 @@ export const BentoGridItem = ({
   description,
   img,
   id,
-  icon
+  icon,
 }: {
   className?: string;
   title?: string | React.ReactNode;
-  id:number;
+  id: number;
   description?: string | React.ReactNode;
   img?: string;
   icon?: React.ReactNode;
@@ -37,11 +37,13 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        "row-span-1 relative w-[85vw] h-[50vh] md:h-[45vh] md:w-[27vw] rounded-2xl group/bento hover:shadow-md transition duration-300 shadow-input dark:shadow-[#3b3b3b] p-4 dark:bg-black dark:border-white/[0.3] bg-green-400 border border-transparent justify-between flexborder-red-500 first-letter:flex flex-col space-y-4",
+        "row-span-1 relative w-[85vw] h-auto md:w-[27vw] rounded-2xl group/bento hover:shadow-md transition duration-300 shadow-input dark:shadow-[#3b3b3b] p-4 dark:bg-black dark:border-white/[0.3]  border border-transparent justify-between flexborder-red-500 first-letter:flex flex-col space-y-4",
         className
       )}
     >
-      {img}
+      <div >
+        <img className="rounded-xl object-cover object-center" src={img} />
+      </div>
       <div className="group-hover/bento:translate-x-2 transition duration-200">
         {icon}
         <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
@@ -54,4 +56,3 @@ export const BentoGridItem = ({
     </div>
   );
 };
-
