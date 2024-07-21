9fe726442fd1959a7222369dcb5b193a36d79a9a
rehanmirza76 <abdulrehan0317@gmail.com>
2024-07-21 18:45:03 +0500

    update 😊

--- portfolio-web/components/ui/floating-navbar.tsx
+++ portfolio-web/components/ui/floating-navbar.tsx
@@ -29,7 +29,7 @@ export const FloatingNav = ({
     if (typeof current === "number") {
       let direction = current! - scrollYProgress.getPrevious()!;
 
-      if (scrollYProgress.get() < 0.) {
+      if (scrollYProgress.get() < 0) {
         setVisible(true);
       } else {
         if (direction < 0) {
@@ -53,7 +53,7 @@ export const FloatingNav = ({
           opacity: visible ? 1 : 0,
         }}
         transition={{
-          duration: 0.2,
+          duration: 0.5,
         }}
         className={cn(
           "flex max-w-fit  fixed top-10 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-xl dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]  z-[5000] px-3 sm:px-5 py-2  items-center justify-center space-x-2 sm:space-x-4 ",
