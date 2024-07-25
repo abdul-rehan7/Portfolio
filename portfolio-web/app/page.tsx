5c38624e8c667964aecc966dd0bd145652546678
rehanmirza76 <abdulrehan0317@gmail.com>
2024-07-25 15:37:07 +0500

    update

--- portfolio-web/app/page.tsx
+++ portfolio-web/app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/floating-navbar";
 import Loader from "@/components/Loader"
 import Grid from "@/components/Grid"
+import RecentProjects from "@/components/RecentProjects"
 
 
 export default function Home() {
@@ -13,6 +14,7 @@ export default function Home() {
         <FloatingNav navItems={[{name: "Home", link: ""},{name: "Projects", link: ""},{name: "About", link: ""},{name: "Contact", link: ""}]}/>
         <Hero />
         <Grid/>
+        <RecentProjects/>
 
         
       </div>
---
+++ portfolio-web/components/RecentProjects.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { projects } from "@/Data";
+import { div } from "three/examples/jsm/nodes/Nodes.js";
+
+const RecentProjects = () => {
+  return (
+    <div className="py-20 z-40">
+      <h1 className="heading">
+        Small selection of {""}
+        <span className="text-purple">Recent Projects</span>
+      </h1>
+      <div className="flex flex-wrap items-center justify-normal p-4 gap-16 mt-10">
+        {projects.map((projects)=>(
+            <div>
+                {projects.title}
+            </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default RecentProjects;
